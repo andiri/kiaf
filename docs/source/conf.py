92490727768974f93e032f3a92aea6d8f1641226
Andiri <realapyo@gmail.com>
2022-05-16 14:52:45 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KIAF User Guide'
+copyright = '2020, GSDC'
+author = 'kong91'
 
-release = '0.1'
-version = '0.1.0'
+release = '2.0'
+version = '2.0.1'
 
 # -- General configuration
 
